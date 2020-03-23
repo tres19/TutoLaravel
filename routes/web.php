684468d1758e5166a3867dc0e98eb30a451bccb2
TRESMONTAN François (AIST87) <ftresmontan@ssti.local>
2020-03-23 11:55:21 +0100

    Routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['as'=>'home', function () {
+    return view('vue1');
+}]);
+
+Route::get('{n}', function($n) {
+    return response('Je suis la page ' . $n .' ! ', 200);
+})->where('n','[1-3]');
+
+
+Route::get('article/{n}', function($n) {
+    return view('article')->withNumero($n);
+})->where('n', '[0-9]+');
+
+Route::get('facture/{n}', function($n) {
+    return view('facture')->withNumero($n);
+})->where('n', '[0-9]+');
